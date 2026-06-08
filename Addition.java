f9aa8bcfde6b98cf4bd39f0bf11d11ce09143bcd
Gupta Durgadevi <141473948+gupta-Durgadevi@users.noreply.github.com>
2026-06-08 12:39:03 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//50//CE: NOT A STATEMET
+		// for storing variable ,createusing data type like int,char,double,boolean
+		int a;
+		int b;
+		int c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println("The addition of"+a+" and "+b+" is "+c);
+
+	}
+
+}
