7add42dbdc11e7187ff4c456551e3b54624255fc
Gupta Durgadevi <141473948+gupta-Durgadevi@users.noreply.github.com>
2026-06-08 12:45:23 +0530

    Add files via upload

---
+++ Lefttriangle.java
@@ -0,0 +1,23 @@
+package pattern;
+
+public class Lefttriangle {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+		        int rows = 5; // number of rows
+
+		        for(int i = 1; i <= rows; i++) {
+		            for(int j = 1; j <= i; j++) {
+		                System.out.print("*");
+		            }
+		            System.out.println(); // move to next line
+		        }
+		    }
+
+	}
+
+
