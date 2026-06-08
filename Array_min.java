c9b89aa5591d05b833ddf6438867e94f84a55588
Gupta Durgadevi <141473948+gupta-Durgadevi@users.noreply.github.com>
2026-06-08 12:41:12 +0530

    Add files via upload

---
+++ Array_min.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Array_min {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {12,34,54,1};
+		int min=arr[0];
+		
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]<min) {
+				min=arr[i];
+			}
+		}
+		
+		System.out.println("min element "+min);
+	}
+
+}
